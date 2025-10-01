ff82f35e9b3cbc166054924c88304027bd0f4796
angel <viciousanrogu@gmail.com>
2025-10-01 14:09:45 -0600

    BASES DEL PROYECTO

---
+++ src/test/java/com/proyecto/SsYPp/SsYPpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.SsYPp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsYPpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
